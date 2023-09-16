cc9cbf11695aa8ea33a32351b25073dcc04d2dd1
0xngmi <0xngmi@protonmail.com>
2023-09-17 00:34:42 +0100

    add tests for usdc

--- test/Subs.ts
+++ test/Subs.ts
@@ -6,15 +6,20 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
+// CHANGE THESE VARIABLES TO TEST WITH DIFFERENT VAULTS AND TOKENS WITH DIFFERENT DECIMALS
 const mainnet = false
-const tokenAddress = mainnet?'0x6B175474E89094C44Da98b954EedeAC495271d0F':'0xda10009cbd5d07dd0cecc66161fc93d7c9000da1'
-const vaultAddress = mainnet?'0x83F20F44975D03b1b09e64809B757c47f942BEeA':'0x85c6Cd5fC71AF35e6941d7b53564AC0A68E09f5C' // sDAI : 4626 aDAI
-const whaleAddress = mainnet?'0x075e72a5edf65f0a5f44699c7654c1a76941ddc8':'0x9cd4ff80d81e4dda8e9d637887a5db7e0c8e007b'
+const useUSDC = true // Requires mainnet === false
 
-const usdcAddress = mainnet?'-':'0x7f5c764cbc14f9669b88837ca1490cca17c31607'
-const usdcVault = mainnet?'-':'0x6E6699E4B8eE4Bf35E72a41fe366116ff4C5A3dF'
 
-const fe = (n:number) => ethers.parseEther(n.toString())
+const tokenAddress = mainnet?'0x6B175474E89094C44Da98b954EedeAC495271d0F':
+  useUSDC?'0x7f5c764cbc14f9669b88837ca1490cca17c31607':'0xda10009cbd5d07dd0cecc66161fc93d7c9000da1'
+const vaultAddress = mainnet?'0x83F20F44975D03b1b09e64809B757c47f942BEeA':
+  useUSDC?'0x6E6699E4B8eE4Bf35E72a41fe366116ff4C5A3dF':'0x85c6Cd5fC71AF35e6941d7b53564AC0A68E09f5C' // sDAI : 4626 aDAI
+const whaleAddress = mainnet?'0x075e72a5edf65f0a5f44699c7654c1a76941ddc8':
+  useUSDC?'0x7f5c764cbc14f9669b88837ca1490cca17c31607':'0x9cd4ff80d81e4dda8e9d637887a5db7e0c8e007b'
+const tokenYield = useUSDC?0.026:0.0209
+
+const fe = (n:number) => ethers.parseUnits(n.toFixed(5), useUSDC?6:18)
 const de = (n:bigint|any) => Number(n)/1e18
 
 async function getSub(call: Promise<any>){
@@ -85,29 +90,6 @@ describe("Subs", function () {
     return { subs, token, owner, subReceiver, feeCollector, daiWhale, vault, otherSubscriber };
   }
 
-  async function deployUsdcFixture() {
-    // Contracts are deployed using the first signer/account by default
-    const [owner, subReceiver, feeCollector, otherSubscriber] = await ethers.getSigners();
-    const daiWhale = await ethers.getImpersonatedSigner(whaleAddress);
-    const token = new ethers.Contract(usdcAddress,[
-        "function balanceOf(address account) external view returns (uint256)",
-        "function approve(address spender, uint256 amount) external returns (bool)",
-        "function transfer(address spender, uint256 amount) external returns (bool)"
-    ], daiWhale)
-    const vault = new ethers.Contract(usdcVault,[
-      "function balanceOf(address account) external view returns (uint256)",
-      "function convertToAssets(uint256 shares) external view returns (uint256)",
-      "function convertToShares(uint256 assets) external view returns (uint256)"
-    ], daiWhale)
-
-    const Subs = await ethers.getContractFactory("Subs");
-    const subs = await Subs.deploy(30*24*3600, usdcAddress, usdcVault, feeCollector.address, await time.latest());
-
-    await token.approve(await subs.getAddress(), fe(1e6))
-
-    return { subs, token, owner, subReceiver, feeCollector, daiWhale, vault, otherSubscriber };
-  }
-
   describe("Basic", function () {
     it("Should work", async function () {
       const { subs, daiWhale, subReceiver } = await loadFixture(deployFixture);
@@ -166,7 +148,9 @@ describe("Subs", function () {
       expect(await token.balanceOf(subReceiver.address)).to.be.eq(0);
       await subs.connect(subReceiver).claim(fe(1)); // update balances info
       await subs.connect(subReceiver).claim((await subs.receiverBalances(subReceiver.address)).balance)
-      expect(await token.balanceOf(subReceiver.address)).to.be.approximately(fe(600*0.99+10), fe(1));
+      const yieldGenerated = 10*(1+tokenYield)**5 - 10
+      expect(await token.balanceOf(subReceiver.address)).to.be.approximately(fe((610+yieldGenerated)*0.99), fe(0.1));
+      expect(await vault.balanceOf(await subs.getAddress())).to.be.approximately(0, 2);
       const receiverBalance = await subs.receiverBalances(subReceiver.address)
       expect(receiverBalance.balance).to.be.eq(0);
       expect(receiverBalance.amountPerPeriod).to.be.eq(fe(10));
@@ -187,9 +171,6 @@ describe("Subs", function () {
     })
 
     it("claim correctly discounts expirations", async function () {
-    })
-
-    it("works well with tokens that have different decimals", async function () {
     })
 
     it("receiver earns yield", async function () {
@@ -239,18 +220,18 @@ describe("Subs", function () {
 
       await time.increase(3*30*24*3600);
       await subs.connect(subReceiver).claim(fe(0.01));
-      otherSubBalance += -3*13 + (91)*0.02*3/12
+      otherSubBalance += -3*13 + (91)*tokenYield*3/12
       expect(await calculateSubBalance(otherSub, subs, await time.latest(), vault, fe(1), 30*24*3600)).to.be.approximately(fe(otherSubBalance), fe(0.1))
       await subs.connect(subReceiver).claim((await subs.receiverBalances(subReceiver.address)).balance);
       expect((await subs.receiverBalances(subReceiver.address)).balance).to.be.eq(0)
       const firstBal = await token.balanceOf(subReceiver.address)
       expect(firstBal).to.be.approximately(fe(7*5+13*3), fe(1))
       await time.increase(1*30*24*3600);
-      otherSubBalance += -1*13 + (otherSubBalance)*0.02*1/12
+      otherSubBalance += -1*13 + (otherSubBalance)*tokenYield*1/12
       expect(await calculateSubBalance(otherSub, subs, await time.latest(), vault, fe(1), 30*24*3600)).to.be.approximately(fe(otherSubBalance), fe(0.1))
       await subs.connect(daiWhale).unsubscribe(...unsubscribeParams(whaleSub))
       await time.increase(10*30*24*3600);
-      otherSubBalance += -3*13 + (otherSubBalance)*0.02*10/12
+      otherSubBalance += -3*13 + (otherSubBalance)*tokenYield*10/12
       expect(await calculateSubBalance(otherSub, subs, await time.latest(), vault, fe(1), 30*24*3600)).to.be.approximately(fe(otherSubBalance), fe(0.1))
       await subs.connect(subReceiver).claim(fe(0.01));
       await subs.connect(subReceiver).claim((await subs.receiverBalances(subReceiver.address)).balance);
@@ -258,7 +239,9 @@ describe("Subs", function () {
       expect(secondBal-firstBal).to.be.approximately(fe(7*1+13*4), fe(1))
       const prevOtherBal = await token.balanceOf(otherSubscriber.address)
       await subs.connect(otherSubscriber).unsubscribe(...unsubscribeParams(otherSub))
-      expect(await token.balanceOf(otherSubscriber.address)-prevOtherBal).to.be.approximately(fe(1.2), fe(0.1))
+      expect(await token.balanceOf(otherSubscriber.address)-prevOtherBal).to.be.approximately(fe(otherSubBalance), fe(0.1))
+      expect(otherSubBalance).to.be.above(1.2)
+      expect(otherSubBalance).to.be.below(1.6)
       expect(await vault.balanceOf(await subs.getAddress())).to.be.approximately(0, 4)
       await time.increase(5*30*24*3600);
       await expect(subs.connect(subReceiver).claim(fe(0.01))).to.be.reverted
