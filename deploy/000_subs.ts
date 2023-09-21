efc95ef7db3a9ed215ecaa0f0b65a48ec3bb75fe
0xngmi <0xngmi@protonmail.com>
2023-09-21 22:51:29 +0100

    update deploy script

--- deploy/000_subs.ts
+++ deploy/000_subs.ts
@@ -6,7 +6,8 @@ const func = async function (hre:any) {
 
   await deploy('Subs', {
     from: deployer,
-    args: [5*60, "0x65343F414FFD6c97b0f6add33d16F6845Ac22BAc", deployer, 1694919024, deployer, "0xf8126ef025651e1b313a6893fcf4034f4f4bd2aa"], // 2023-9-1, 12:00:00 am UTC
+    args: [5*60, "0x65343F414FFD6c97b0f6add33d16F6845Ac22BAc", deployer, 1694919024, // 2023-9-1, 12:00:00 am UTC
+      deployer, "0xf8126ef025651e1b313a6893fcf4034f4f4bd2aa", "1000"+"000000000000000000"],
     log: true,
     autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
     //deterministicDeployment: true,
@@ -14,4 +15,3 @@ const func = async function (hre:any) {
 };
 module.exports = func;
 func.tags = ['Subs'];
-func.dependencies = ['YearnERC4626'];
