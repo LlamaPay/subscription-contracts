1ee0a61585e3f11fd643d627343879ab96e92343
0xngmi <0xngmi@protonmail.com>
2023-09-29 23:55:19 +0100

    avoid reverts

--- test/Subs.ts
+++ test/Subs.ts
@@ -18,7 +18,7 @@ async function calculateSubBalance(sub: any, subs: any, currentTimestamp: number
     }
     const sharesPaid = ((sharesAccumulator - sub.accumulator) * sub.amountPerCycle) / (DIVISOR as any);
     const sharesLeft = sub.initialShares - sharesPaid;
-    return vault.convertToAssets(sharesLeft)
+    return sharesLeft < 0? 0n:vault.convertToAssets(sharesLeft)
   } else {
     const periods = []
     for (let period = sub.initialPeriod; period < sub.expirationDate; period += BigInt(periodDuration)) {
@@ -32,7 +32,8 @@ async function calculateSubBalance(sub: any, subs: any, currentTimestamp: number
     periodShares.forEach((shares) => {
       subsetAccumulator += shares === 0n ? currentSharePrice : shares;
     })
-    return vault.convertToAssets(sub.initialShares - ((subsetAccumulator * (sub.amountPerCycle as bigint)) / DIVISOR));
+    const shares = sub.initialShares - ((subsetAccumulator * (sub.amountPerCycle as bigint)) / DIVISOR)
+    return shares < 0? 0n:vault.convertToAssets(shares);
   }
 }
 
