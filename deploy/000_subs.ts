ddaef19cf2cbd3defd7414decd369160e211999b
0xngmi <0xngmi@protonmail.com>
2023-10-13 22:20:53 +0100

    use current time

--- deploy/000_subs.ts
+++ deploy/000_subs.ts
@@ -6,7 +6,7 @@ const func = async function (hre:any) {
 
   await deploy('Subs', {
     from: deployer,
-    args: [5*60, "0x65343F414FFD6c97b0f6add33d16F6845Ac22BAc", deployer, 1694919024, // 2023-9-1, 12:00:00 am UTC
+    args: [24*60*60, "0x65343F414FFD6c97b0f6add33d16F6845Ac22BAc", deployer, Math.floor(Date.now()/1e3),
       deployer, "0xf8126ef025651e1b313a6893fcf4034f4f4bd2aa", "1000"+"000000000000000000"],
     log: true,
     autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
